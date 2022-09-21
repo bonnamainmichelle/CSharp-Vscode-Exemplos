7115b17a257e45daaacbfee78d6bd0123b4422c3
Michelle Bonnamain <bonnamainmichelle@gmail.com>
2022-09-21 16:59:15 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string nome;
+
+        Console.WriteLine("Qual é o seu nome? ");
+        nome = Console.ReadLine();
+    }
+}
